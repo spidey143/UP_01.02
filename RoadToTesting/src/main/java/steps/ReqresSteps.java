3b2acd6627380428569f2efbf1e40a4fe8ec2095
spidey143 <bekenev.daniilkpc582020@gmail.com>
2023-10-24 14:53:36 +0300

    API reqres DONE!

--- RoadToTesting/src/main/java/steps/ReqresSteps.java
+++ RoadToTesting/src/main/java/steps/ReqresSteps.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static io.restassured.RestAssured.*;
 
 public class ReqresSteps {
-    @Step
+    @Step("Получить список пользователей")
     public void getUsersList() {
         List<DataResponse> dataResponseList = given().when().spec(requestSpecification)
                 .get("api/users?page=2")
@@ -23,7 +23,30 @@ public class ReqresSteps {
         dataResponseList.forEach(u -> Assert.assertTrue(u.avatar.contains(u.id.toString())));
     }
 
-    @Step
+    @Step("Получить список ресурсов")
+    public void getListResource() {
+         given().when().spec(requestSpecification)
+                .get("api/unknown")
+                .then().statusCode(200).log().all()
+                .extract().body().jsonPath().getList("data", ResourceDataResponse.class);
+    }
+
+    @Step("Получить ресурс")
+    public void getSingleResource() {
+        given().when().spec(requestSpecification)
+                .get("api/unknown/2")
+                .then().statusCode(200).log().all()
+                .extract().body().as(ResourceResponse.class);
+    }
+
+    @Step("Получить несуществующего пользователя")
+    public void getNotExistResource() {
+        given().when().spec(requestSpecification)
+                .get("api/unknown/23")
+                .then().statusCode(404).log().all();
+    }
+
+    @Step("Получить пользователя по его id")
     public void getUserById(Integer id, Integer code, UserResponse expectedUserResponse) {
         UserResponse userResponse = given()
                 .when().spec(requestSpecification)
@@ -33,7 +56,7 @@ public class ReqresSteps {
         Assert.assertEquals(userResponse, expectedUserResponse);
     }
 
-    @Step
+    @Step("Создание пользователя")
     public void createUser(JobRequest user) {
         JobResponse jobResponse = given().spec(requestSpecification)
                 .when()
@@ -43,7 +66,7 @@ public class ReqresSteps {
         Assert.assertTrue(jobResponse.equals(new JobResponse("morpheus", "leader")));
     }
 
-    @Step
+    @Step("Обновление данных о пользователе")
     public void updateUser(JobRequest user, String method) {
         JobResponse jobResponse = given().spec(requestSpecification)
                 .when()
@@ -53,7 +76,7 @@ public class ReqresSteps {
         Assert.assertEquals(jobResponse.job, "zion resident");
     }
 
-    @Step
+    @Step("Удалить пользователя")
     public void deleteUser() {
         given().spec(requestSpecification)
                 .when()
@@ -61,7 +84,7 @@ public class ReqresSteps {
                 .then().statusCode(204).log().all();
     }
 
-    @Step
+    @Step("Регистрация пользователя")
     public void registerUser(RegisterRequest registerRequest, Integer code) {
         RegisterResponse registerResponse = given().spec(requestSpecification)
                 .when()
@@ -73,7 +96,7 @@ public class ReqresSteps {
         else Assert.assertEquals(registerResponse.error, "Missing password");
     }
 
-    @Step
+    @Step("Авторизация пользователя")
     public void loginUser(LoginRequest loginRequest, Integer code) {
         LoginResponse loginResponse = given().spec(requestSpecification)
                 .when()
@@ -83,5 +106,14 @@ public class ReqresSteps {
         if (loginResponse.error == null)
             Assert.assertEquals(loginResponse.token, "QpwL5tke4Pnpja7X4");
         else Assert.assertEquals(loginResponse.error, "Missing password");
+    }
+
+    @Step("Получить отложенный список пользователей")
+    public void getDelayUserList() {
+        List<DataResponse> dataResponseList = given().when().spec(requestSpecification)
+                .get("api/users?delay=3")
+                .then().statusCode(200).log().all()
+                .extract().body().jsonPath().getList("data", DataResponse.class);
+        dataResponseList.forEach(u -> Assert.assertTrue(u.avatar.contains(u.id.toString())));
     }
 }
--- RoadToTesting/src/test/java/ReqresTests.java
+++ RoadToTesting/src/test/java/ReqresTests.java
@@ -2,25 +2,25 @@
 import base.BaseTest;
 import io.qameta.allure.Description;
 import io.qameta.allure.Story;
+import io.restassured.RestAssured;
+import io.restassured.config.HttpClientConfig;
+import io.restassured.config.RestAssuredConfig;
 import model.ReqresUserData;
 import model.requestModel.JobRequest;
 import model.requestModel.LoginRequest;
 import model.requestModel.RegisterRequest;
 import model.responseModel.*;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import java.util.Date;
-import java.util.List;
 
-public class ReqresTests extends BaseTest{
+public class ReqresTests extends BaseTest {
 
-    @Test(testName ="GET List Users", description = "Проверка что аватар содержит id пользователя")
+    @Test(testName = "GET List Users", description = "Проверка что аватар содержит id пользователя")
     @Story("GET requests")
     public static void getUsersList() {
         REQRES_STEPS.getUsersList();
     }
 
-    @Test(testName ="GET Single Users",description = "Получить пользователя по его айди")
+    @Test(testName = "GET Single Users", description = "Получить пользователя по его id")
     @Story("GET requests")
     public void getUserById() {
         DataResponse dataResponse = new DataResponse(
@@ -33,66 +33,91 @@ public class ReqresTests extends BaseTest{
         REQRES_STEPS.getUserById(2, 200, expectedResponse);
     }
 
-    @Test(testName ="GET Single Users Not Found", description = "Получение несуществующего пользователя")
+    @Test(testName = "GET Single Users NOT FOUND", description = "Получение несуществующего пользователя")
     @Story("GET requests")
     public static void getNotExistUser() {
         REQRES_STEPS.getUserById(23, 404, new UserResponse());
     }
 
-    @Test(testName ="POST Create", description = "Создание пользователя",
+    @Test(testName = "GET List Resource", description = "Получение списка ресурсов")
+    @Story("GET requests")
+    public static void getListResource() {
+        REQRES_STEPS.getListResource();
+    }
+
+    @Test(testName = "GET Single Resource", description = "Получение ресурса")
+    @Story("GET requests")
+    public static void getSingleResource() {
+        REQRES_STEPS.getSingleResource();
+    }
+
+    @Test(testName = "GET Single Resource NOT FOUND", description = "Получение несуществующего ресурса")
+    @Story("GET requests")
+    public static void getNotExistResource() {
+        REQRES_STEPS.getNotExistResource();
+    }
+
+    @Test(testName = "POST Create", description = "Создание пользователя",
             groups = "POST requests")
     @Story("POST requests")
-    public void createUser(){
+    public void createUser() {
         JobRequest user = new JobRequest("morpheus", "leader");
         REQRES_STEPS.createUser(user);
     }
 
-    @Test(testName ="PUT Update", description = "Обновление пользователя")
+    @Test(testName = "PUT Update", description = "Обновление пользователя")
     @Story("UPDATE requests")
     @Description("Обновление пользователя c использованием PUT")
-    public static void putUpdateUser(){
+    public static void putUpdateUser() {
         JobRequest user = new JobRequest("morpheus", "zion resident");
         REQRES_STEPS.updateUser(user, "put");
     }
 
-    @Test(testName ="PATCH Update", description = "Обновление пользователя")
+    @Test(testName = "PATCH Update", description = "Обновление пользователя")
     @Story("UPDATE requests")
     @Description("Обновление пользователя c использованием PATCH")
-    public static void patchUpdateUser(){
+    public static void patchUpdateUser() {
         JobRequest user = new JobRequest("morpheus", "zion resident");
         REQRES_STEPS.updateUser(user, "patch");
     }
 
-    @Test(testName ="DELETE", description = "Удаление пользователя")
+    @Test(testName = "DELETE", description = "Удаление пользователя")
     @Story("DELETE request")
-    public static void deleteUser(){
+    public static void deleteUser() {
         REQRES_STEPS.deleteUser();
     }
 
-    @Test(testName ="POST Register-successful", description = "Успешная регистрация пользователя")
+    @Test(testName = "POST Register-successful", description = "Успешная регистрация пользователя")
     @Story("POST requests")
-    public static void registerSuccessful(){
+    public static void registerSuccessful() {
         RegisterRequest registerRequest = new RegisterRequest("eve.holt@reqres.in", "pistol");
         REQRES_STEPS.registerUser(registerRequest, 200);
     }
 
-    @Test(testName ="POST Register-unsuccessful", description = "Провальная регистрация пользователя")
+    @Test(testName = "POST Register-unsuccessful", description = "Провальная регистрация пользователя")
     @Story("POST requests")
-    public static void registerUnSuccessful(){
+    public static void registerUnSuccessful() {
         RegisterRequest registerRequest = new RegisterRequest("eve.holt@reqres.in");
         REQRES_STEPS.registerUser(registerRequest, 400);
     }
 
-    @Test(testName ="POST Login-successful", description = "Успешная авторизация пользователя")
+    @Test(testName = "POST Login-successful", description = "Успешная авторизация пользователя")
     @Story("POST requests")
-    public static void loginSuccessful(){
+    public static void loginSuccessful() {
         LoginRequest loginRequest = new LoginRequest("eve.holt@reqres.in", "cityslicka");
         REQRES_STEPS.loginUser(loginRequest, 200);
     }
-    @Test(testName ="POST Login-unsuccessful", description = "Провальная авторизация пользователя")
+
+    @Test(testName = "POST Login-unsuccessful", description = "Провальная авторизация пользователя")
     @Story("POST requests")
-    public static void loginUnSuccessful(){
+    public static void loginUnSuccessful() {
         LoginRequest loginRequest = new LoginRequest("eve.holt@reqres.in");
         REQRES_STEPS.loginUser(loginRequest, 400);
+    }
+
+    @Test(testName = "GET delayed RESPONSE", description = "Проверка что аватар содержит id пользователя")
+    @Story("GET requests")
+    public static void getDelayedResp() {
+        REQRES_STEPS.getUsersList();
     }
 }
